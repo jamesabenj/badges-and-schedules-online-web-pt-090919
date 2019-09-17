50937f612454fd657600aea752426775f9e62b5b
James Benjamin <james.abenj@gmail.com>
2019-09-17 18:45:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,8 +23,8 @@ end
 
 def printer(attendees)
   attendees.each do |name|
-  puts batch_badge_creator(array)
-  puts assign_rooms(array)
+  puts batch_badge_creator(attendees)
+  puts assign_rooms(attendees)
 end
   
 end
