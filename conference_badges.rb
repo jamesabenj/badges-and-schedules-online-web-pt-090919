12774bd01330034c8ca1a05089d54adb2c418ec1
James Benjamin <james.abenj@gmail.com>
2019-09-17 18:01:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,42 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  new_array = []
+  array.each do |name|
+  new_array.push("Hello, my name is #{name}.")
+end
+return new_array
+end
+
+def assign_rooms(array)
+  counter = 1
+  new_array = []
+  array.each do |name|
+    new_array.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter += 1
+  end
+  return new_array
+end
+
+def printer(batch_badge_creator, assign_rooms)
+  
+  batch_badge_creator.each do |badges|
+    counter = 0
+  puts  batch_badge_creator[counter]
+  counter += 1
+  end
+  
+  assign_rooms.each do |assignments|
+    counter = 0
+  puts assign_rooms[counter]
+  counter += 1
+  end
+  
+end
+
+  
+  
+  
