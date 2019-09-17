438978da1bf7d27c8511d71d43b802b6c529838a
James Benjamin <james.abenj@gmail.com>
2019-09-17 18:37:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,23 +23,9 @@ end
 
 def printer(array)
   array.each do |name|
-  puts ("Hello, my name is #{name}.")
+  puts batch_badge_creator(array)
+  puts assign_rooms(array)
 end
-
-
-end
-  
-  batch_badge_creator(array).each do |badges|
-  puts  badges
-  end
-  
-  assign_roomarray).each do |assignments|
-  array.each do |name|
-    new_array.push("Hello, #{name}! You'll be assigned to room #{counter}!")
-    counter += 1
-  end
-  puts assignments
-  end
   
 end
 
